1290289ca5e3efa85ea4e9f4335c5a095f9a4085
Joseph <jostheblessing@gmail.com>
2019-04-23 08:32:07 +0200

    [Chore #165540427] Host on heroku

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,15 @@
-import pool from '../config/connection';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_DEV,
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
 
 export const createTables = () => {
   const users = `CREATE TABLE IF NOT EXISTS
