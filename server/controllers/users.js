00ce29484863c5e556a181c0b69ea957cf28a3df
Joseph <jostheblessing@gmail.com>
2019-04-25 17:54:13 +0200

    [Feature #165466742] Allow user to login

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import db from '../models/queries';
 import { validateUser, validateUserLogin } from '../helpers/user_validation';
 
@@ -34,5 +35,41 @@ exports.signup = async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ status: 500, error: err });
+  }
+};
+
+// SIGNIN
+exports.login = async (req, res) => {
+  try {
+    const { error } = validateUserLogin(req.body);
+    if (error) return res.status(400).json({ status: 400, error: error.details[0].message });
+
+    const user = await db.fetchOneUser(req.body);
+    if (!user.rows[0]) {
+      return res.status(404).json({ status: 404, error: 'Email and password not found' });
+    }
+
+    const compare = bcrypt.compareSync(req.body.password, user.rows[0].password);
+    if (compare) {
+      const payload = {
+        id: user.rows[0].id,
+        firstname: user.rows[0].firstname,
+        lastname: user.rows[0].lastname,
+        email: user.rows[0].email,
+        type: user.rows[0].type,
+        isAdmin: user.rows[0].isAdmin,
+      };
+      const token = jwt.sign(payload, process.env.secretKey, { expiresIn: '24h' });
+      return res.status(200).json({
+        status: 200,
+        data: token,
+        id: payload.id,
+        firstName: payload.firstname,
+        lastName: payload.lastname,
+        email: payload.email,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ status: 500, error: err });
   }
 };
