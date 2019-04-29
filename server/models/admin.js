3f241bbe2219d280638d90a01012db2221590224
Joe-Joseph <44877921+Joe-Joseph@users.noreply.github.com>
2019-04-29 08:05:21 +0200

    Update admin.js

--- server/models/admin.js
+++ server/models/admin.js
@@ -10,7 +10,7 @@ const admin = {
   isAdmin: 'true',
 };
 
-const Addadmin = 'INSERT INTO users (email, firstname, lastname, password, type, isAdmin) VALUES($1,$2,$3,$4,$5,$6)';
+const Addadmin = 'INSERT INTO users (email, firstname, lastname, password, type, isAdmin) VALUES($1,$2,$3,$4,$5,$6)ON CONFLICT DO NOTHING';
 pool.query(Addadmin, [admin.email, admin.firstName, admin.lastName, admin.password, admin.type, admin.isAdmin])
   .then((result) => {
     console.log('Admin created');
